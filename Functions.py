712899760b5cea5ef1be4017e5544f558c24d2a8
LukeDude200 <croslynlc@gmail.com>
2021-10-19 00:10:53 -0300

    Primitive Set

--- Functions.py
+++ Functions.py
@@ -4,3 +4,23 @@ import random
 import numpy
 import matplotlib.pyplot as plt
 from deap import algorithms, base, creator, tools, gp
+
+
+def make_prim_set():
+    def protectedDiv(left, right):
+        try:
+            return left / right
+        except ZeroDivisionError:
+            return 1
+
+    primset = gp.PrimitiveSet("MAIN", 1)
+    primset.addPrimitive(operator.add, 2)
+    primset.addPrimitive(operator.sub, 2)
+    primset.addPrimitive(operator.mul, 2)
+    primset.addPrimitive(protectedDiv, 2)
+    primset.addPrimitive(operator.neg, 1)
+    primset.addPrimitive(math.cos, 1)
+    primset.addPrimitive(math.sin, 1)
+    primset.addEphemeralConstant("rand101", lambda: random.randint(-1, 1))
+    primset.renameArguments(ARG0='x')
+
