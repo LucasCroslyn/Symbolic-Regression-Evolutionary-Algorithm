6f0e7c2cd85bb04501758a281bc795ef2192ebcd
LukeDude200 <croslynlc@gmail.com>
2021-10-18 23:52:26 -0300

    Imports

---
+++ Functions.py
@@ -0,0 +1,6 @@
+import operator
+import math
+import random
+import numpy
+import matplotlib.pyplot as plt
+from deap import algorithms, base, creator, tools, gp
--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-import operator
-import math
-import random
-import numpy
-import matplotlib.pyplot as plt
-from deap import algorithms, base, creator, tools, gp
+from Functions import *
 
