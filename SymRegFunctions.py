fcec9951b023656ed7fc01ed65afd9d6b67ccbe8
LukeDude200 <croslynlc@gmail.com>
2021-11-07 18:39:54 -0400

    Plotting of points done (limit it to 2d/3d still need to do)

--- SymRegFunctions.py
+++ SymRegFunctions.py
@@ -82,14 +82,21 @@ def symReg(independent, dependent):
     pop, log = algorithms.eaSimple(pop, toolbox, 0.3, 0.05, 200, stats=mstats, halloffame=hof, verbose=True)
     best = hof.items[0]
     print(best)
-    print(len(best))
     nodes, edges, labels = gp.graph(best)
     g = nx.Graph()
     g.add_nodes_from(nodes)
     g.add_edges_from(edges)
     pos = nx.nx_agraph.graphviz_layout(g, prog="dot")
-
     nx.draw_networkx_nodes(g, pos)
     nx.draw_networkx_edges(g, pos)
     nx.draw_networkx_labels(g, pos, labels)
+    plt.show()
+    best_y = []
+    bestFunction = toolbox.compile(expr=best)
+    for i in range(len(independent)):
+        best_y.append(bestFunction(independent[i])[0])
+    print(best_y)
+
+    plt.plot(independent, best_y, color='orange', linewidth=0.5)
+    plt.scatter(independent, dependent, s=5)
     plt.show()
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from SymRegFunctions import *
 data = get_data("d1.csv")
 independent_variables = data[:, :-1]
 dependent_variables = data[:, -1]
+print(independent_variables)
 symReg(independent_variables, dependent_variables)
-
+#plt.plot(independent_variables, dependent_variables, "ob")
+#plt.show()
